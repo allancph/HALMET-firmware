ff39ccfb5d23c1838944db12df06f97836345c54
allancph <allancph@gmail.com>
2024-03-19 10:41:22 +0100

    added 1.try

--- src/main.cpp
+++ src/main.cpp
@@ -311,6 +311,56 @@ if (display_present) {
     });
   }
 
+
+/// 1wire temperature sensor
+
+  uint8_t pin = 4;
+
+  DallasTemperatureSensors* dts = new DallasTemperatureSensors(pin);
+
+  // Define how often SensESP should read the sensor(s) in milliseconds
+  uint read_delay = 500;
+
+  // Below are temperatures sampled and sent to Signal K server
+  // To find valid Signal K Paths that fits your need you look at this link:
+  // https://signalk.org/specification/1.4.0/doc/vesselsBranch.html
+
+  // Measure coolant temperature
+  auto* coolant_temp =
+      new OneWireTemperature(dts, read_delay, "/coolantTemperature/oneWire");
+
+  coolant_temp->connect_to(new Linear(1.0, 0.0, "/coolantTemperature/linear"))
+      ->connect_to(new SKOutputFloat("propulsion.mainEngine.coolantTemperature",
+                                     "/coolantTemperature/skPath"));
+
+  // Measure exhaust temperature
+  //auto* exhaust_temp =
+  //    new OneWireTemperature(dts, read_delay, "/exhaustTemperature/oneWire");
+
+  //exhaust_temp->connect_to(new Linear(1.0, 0.0, "/exhaustTemperature/linear"))
+  //    ->connect_to(new SKOutputFloat("propulsion.mainEngine.exhaustTemperature",
+  //                                   "/exhaustTemperature/skPath"));
+
+  // Measure temperature of 24v alternator
+  //auto* alt_24v_temp =
+  //    new OneWireTemperature(dts, read_delay, "/24vAltTemperature/oneWire");
+
+  //alt_24v_temp->connect_to(new Linear(1.0, 0.0, "/24vAltTemperature/linear"))
+  //    ->connect_to(new SKOutputFloat("electrical.alternators.24V.temperature",
+  //                                   "/24vAltTemperature/skPath"));
+
+  // Measure temperature of 12v alternator
+  //auto* alt_12v_temp =
+  //   new OneWireTemperature(dts, read_delay, "/12vAltTemperature/oneWire");
+
+  //alt_12v_temp->connect_to(new Linear(1.0, 0.0, "/12vAltTemperature/linear"))
+  //    ->connect_to(new SKOutputFloat("electrical.alternators.12V.temperature",
+  //                                   "/12vAltTemperature/skPath"));
+
+  // Configuration is done, lets start the readings of the sensors!
+
+
+
   ///////////////////////////////////////////////////////////////////
   // Start the application
 
